8a161c5e7179990d8a91bad0f19913e7122a50e1
saeed <mostafazarei13751375@gmail.com>
2023-07-17 02:20:53 +0430

    css new code

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let useerInput= document.getElementById("userinput")
 useerInput.max= new Date().toISOString().split("T")[0];
 let result =  document.getElementById("result")
-
+const resultButton = document.getElementById('button')
 function calculateAge(){
     let birthDate= new Date(useerInput.value)
     let dayBirth= birthDate.getDate();
@@ -18,25 +18,34 @@ function calculateAge(){
     let day=Math.abs(todayDate-dayBirth)
 
     
-    result.innerHTML=  `your  age  is ${years} year and   ${month} months and  ${day}  day old `
+    result.innerHTML=  `<p>Your  Age  Is <span  class='numone'>${years}</span> Year And  <span  class='numtwo'> ${month} </span>Months And <span  class='numthree'> ${day}</span>  Day Old </p>`
+    result.style.border= "solid var(--glow-color)"
+    result.style.borderRadius="0.45em"
+    result.style.padding=" 0.35em 1em"
+    result.style.color="var(--glow-color)"
+    result.style.boxShadow="inset 0px 0px 0.5em 0px var(--glow-color)",
+    "0px 0px 0.5em 0px var(--glow-color)"
+   
 }
 useerInput.addEventListener('keydown',(e)=>{
-    e.preventDefault()
+  e.preventDefault()
 })
 useerInput.addEventListener('click',()=>{
-    useerInput.showPicker()
+  useerInput.showPicker()
 })
 
 
 function padTo2Digits(num) {
-    return num.toString().padStart(2, '0');
-  }
-  
-  function formatDate(date = new Date()) {
-    return [
-      date.getFullYear(),
-      padTo2Digits(date.getMonth() + 1),
-      padTo2Digits(date.getDate()),
-    ].join('-');
-  }
+  return num.toString().padStart(2, '0');
+}
+
+function formatDate(date = new Date()) {
+  return [
+    date.getFullYear(),
+    padTo2Digits(date.getMonth() + 1),
+    padTo2Digits(date.getDate()),
+  ].join('-');
+}
 useerInput.value= formatDate()
+
+// resultButton.click()
